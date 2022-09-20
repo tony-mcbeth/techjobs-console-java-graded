236bc2d1b743d69836219981023cfa1e0df7216a
tony-mcbeth <twmcbeth@gmail.com>
2022-09-20 13:52:03 -0500

    final

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,7 +112,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -123,13 +123,17 @@ public class TechJobs {
             System.out.print("No Results");
         } else {
             for (HashMap<String, String> job : someJobs) {
-                System.out.println();
+                System.out.println("\n*****");
                 for (Map.Entry<String, String> fields : job.entrySet()) {
                     System.out.println(fields.getKey() + ": " + fields.getValue());
                 }
                 // System.out.println("printJobs is not implemented yet");
+                System.out.println("*****");
             }
+
         }
 
-    }//revised
+        System.out.println("No Results");
+    }
 }
+
